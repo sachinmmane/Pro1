b13595ed33453e81d54d533285ae275522846265
sachinmmane <tosachinmane@gmail.com>
2020-03-31 21:57:38 +0530

    Created required component, Imported RouterModule, Install nb-bootstrap/nb-bootstrap (version- 1.0.0beta.1), imported NgbModule, added (nbgDropdown, ngbDropdowntoggel, ngbDropdownMenu).Dropdown is working now.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,15 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyD4ACnlcwtIP-UJfM3lhFhYBn6jCpbiIDA",
+    authDomain: "pro1-1f4a9.firebaseapp.com",
+    databaseURL: "https://pro1-1f4a9.firebaseio.com",
+    projectId: "pro1-1f4a9",
+    storageBucket: "pro1-1f4a9.appspot.com",
+    messagingSenderId: "324154730530",
+    appId: "1:324154730530:web:d2e4ccf018d0bbe9fb427b",
+    measurementId: "G-P1FGMGEZ95"
+  }
 };
